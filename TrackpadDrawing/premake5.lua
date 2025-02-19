abc49de5076e4179ae279da84d1518ccd6782965
Daniel Quednow <danquednow@gmail.com>
2025-02-19 09:04:30 -0500

    Add GLFW to TrackpadDrawing

--- TrackpadDrawing/premake5.lua
+++ TrackpadDrawing/premake5.lua
@@ -2,6 +2,14 @@ project "TrackpadDrawing"
     kind "ConsoleApp"
     language "C++"
 
+    links {
+        "GLFW"
+    }
+
+    includedirs {
+        "../GLFW/include/"
+    }
+
     files {
         "src/**.cpp",
         "src/**.h"
--- TrackpadDrawing/src/main.cpp
+++ TrackpadDrawing/src/main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
 
+#include <GLFW/glfw3.h>
+
 int main() {
     std::cout << "Hello World!" << std::endl;
+
+    glfwInit();
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "Note Window", nullptr, nullptr);
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+
+        glfwSwapBuffers(window);
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
 }
